d956f25e4154d1699c88dd23abb294942fba8286
Leandro Fernandes <leo_fernandes@hotmail.com>
2019-11-03 19:52:42 -0200

    Entidades básicas do modelo

---
+++ Comercial (Hibernate)/src/modelo/Cliente.java
@@ -0,0 +1,32 @@
+package modelo;
+
+public class Cliente {
+
+	private int codigo;
+	private String nome;
+	
+	public Cliente() {
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente: " + nome + "(" + codigo + ")";
+	}
+	
+}
---
+++ Comercial (Hibernate)/src/modelo/Produto.java
@@ -0,0 +1,41 @@
+package modelo;
+
+public class Produto {
+
+	private int codigo;
+	private String descricao;
+	private int quantidadeEmEstoque;
+
+	public Produto() {
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public int getQuantidadeEmEstoque() {
+		return quantidadeEmEstoque;
+	}
+
+	public void setQuantidadeEmEstoque(int quantidadeEmEstoque) {
+		this.quantidadeEmEstoque = quantidadeEmEstoque;
+	}
+
+	@Override
+	public String toString() {
+		return "(" + codigo + ") " + descricao;
+	}
+
+}
